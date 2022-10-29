02bf8d6b927c1588a3b038689d144e969367eebc
Mario Sanz <mariosanzvilla@gmail.com>
2022-10-29 22:03:58 +0200

    saludo 2

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -4,7 +4,7 @@ import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
   app.get("/", (req: Request, res: Response) => {
-    return res.status(200).send("Qué pasa!!");
+    return res.status(200).send("Qué pasa Heroku!!");
   });
 
   app.get("/products", (req: Request, res: Response) => {
