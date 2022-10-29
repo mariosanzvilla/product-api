6a70a2bb7727d1fd1a2566f1b2e598c32b0ec913
Mario Sanz <mariosanzvilla@gmail.com>
2022-10-29 21:55:15 +0200

    saludo

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,6 +3,10 @@ import { Application, Request, Response } from "express";
 import CoursesData from "../../data/courses.json";
 
 export const loadApiEndpoints = (app: Application): void => {
+  app.get("/", (req: Request, res: Response) => {
+    return res.status(200).send("Qué pasa!!");
+  });
+
   app.get("/products", (req: Request, res: Response) => {
     return res.status(200).send(CoursesData);
   });
